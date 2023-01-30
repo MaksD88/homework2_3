d8d4969501e0884b47dd9f08202ec43ea067f00d
Max Doroshkin <d.maks.1883@gmail.com>
2023-01-30 15:16:02 +0300

    initial commit2

---
+++ src/BonusMileService.java
@@ -0,0 +1,8 @@
+public class BonusMileService {
+    public int calculate(int cost) {
+        int miles = cost / 20;
+        return miles;
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusMileService service = new BonusMileService();
+        int price = 1000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
+    }
+
+
+}
+
+
+
