1ae75470702cb716102108a7bc963fe7c2259306
MaksD88 <123211224+MaksD88@users.noreply.github.com>
2023-01-31 11:44:27 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BonusMileService service = new BonusMileService();
-        int price = 1000;
+        int price = 10_000;
         int miles = service.calculate(price);
-         System.out.println("Вам будет начислено " + miles + " миль за купленный билет.");
+        System.out.println(miles);
     }
-
-
 }
-
-
-
